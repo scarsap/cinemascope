40e7f8425643dcfc3a306a7d817413fd56368338
scarsap <scarlettsapieha@gmail.com>
2026-05-26 16:11:19 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/cinemascope",
+  assetPrefix: "/cinemascope",
 };
 
 export default nextConfig;
